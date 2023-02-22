9dd450009d2937b79751639c8b72ab717a70e43f
Daniel Lovette <5632199+dfunklove@users.noreply.github.com>
2023-02-22 16:54:51 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='sparks_graph',
+    version='1.0',
+    author='Daniel Lovette',
+    author_email='dfunklove@gmail.com',
+    url='https://github.com/dfunklove/sparks_graph',
+    license='LICENSE.txt',
+    description='GraphQL back-end for Sparksync',
+    long_description=open('README.md').read(),
+    packages=['sparks_graph'],
+    install_requires=['django', 'django-cors-headers', 'django-extensions', 'django-use-email-as-username', 'strawberry-graphql-django', 'strawberry-django-plus', 'strawberry-django-jwt']
+    )
